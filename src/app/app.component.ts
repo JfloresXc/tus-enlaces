d5e707113e70e39fbe01f9a78023c60044cd4df0
jfloresxc <jfloresxc@gmail.com>
2024-04-17 23:00:42 -0500

    Add - Implementación de template Salient de TailwindCss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormLoginComponent } from './components/form-login/form-login.component';
 import { ListLinksComponent } from './components/list-links/list-links.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormLoginComponent, ListLinksComponent],
+  imports: [
+    RouterOutlet,
+    FormLoginComponent,
+    ListLinksComponent,
+    NavbarComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
